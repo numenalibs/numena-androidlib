b2f99f0dc135528d141442fd4ed87659eca6ae05
tobymohr <tobiasmohr22@gmail.com>
2017-06-12 12:13:43 +0200

    Numena debug

---
+++ numena-lib/src/main/java/numenalibs/co/numenalib/NumenaLibDebug.java
@@ -0,0 +1,12 @@
+package numenalibs.co.numenalib;
+
+import android.util.Log;
+
+public class NumenaLibDebug {
+
+    private static final String TAG ="NUMENALIB";
+
+    public static void d(String message){
+        Log.d(TAG,message);
+    }
+}
