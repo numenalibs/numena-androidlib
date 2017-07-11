49d3ee9f69b72ad75ca11c499d8701030a1a8aa3
tobymohr <tobiasmohr22@gmail.com>
2017-07-11 14:40:15 +0200

    deleted numenaprovider

--- numena-lib/src/main/java/numenalibs/co/numenalib/tools/ValuesManager.java
+++ numena-lib/src/main/java/numenalibs/co/numenalib/tools/ValuesManager.java
@@ -27,7 +27,6 @@ public class ValuesManager implements NumenaCommunicatorInterface {
     private byte[] organisationId = "Numena".getBytes();
     private static ValuesManager instance;
     private DatabaseHelper databaseHelper;
-    private NumenaProvider dataManager;
 
     public static ValuesManager getInstance(){
         if(instance == null){
