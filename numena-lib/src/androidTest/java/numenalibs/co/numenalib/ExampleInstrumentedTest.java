b9d47c6828ff6152ca2dd7d12e01ffcf84f339d7
tobymohr <tobiasmohr22@gmail.com>
2017-08-17 11:36:17 +0200

    corrected test

--- numena-lib/src/androidTest/java/numenalibs/co/numenalib/ExampleInstrumentedTest.java
+++ numena-lib/src/androidTest/java/numenalibs/co/numenalib/ExampleInstrumentedTest.java
@@ -35,7 +35,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-        assertEquals("numenalibs.co.numenalib.test", appContext.getPackageName());
         Looper.prepare();
         numena = Numena.getInstance();
         numena.setupNumenaLibrary(appContext);
